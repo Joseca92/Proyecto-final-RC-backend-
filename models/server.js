b1929c131588c244c5655407278b831bdfca1fc2
JuanMaParedes <juan.manuel.paredes@hotmail.com>
2022-08-06 16:26:04 -0300

    Creacion de controladores y rutas para registro y autentificacion de usuarios y roles

--- models/server.js
+++ models/server.js
@@ -1,19 +1,46 @@
 const express = require("express");
-const { dbConnection } = require("../database/config");
+const cors = require("cors");
+const {dbConnection} = require('../database/config');
 
 class Server{
     constructor() {
         this.app = express();
-        this.port = process.env.port
+        this.port = process.env.PORT;
+        this.usuariosPath='/api/usuarios';
+        this.authPath= '/api/auth';
+        //conexion DB
         this.conectarDB();
+        //middlewares
+        this.middlewares();
+        //routes
+        this.routes();
     }
 
     async conectarDB() {
         await dbConnection();
       }
-        listen(){
+    
+    middlewares(){
+        //Lectura de body
+        this.app.use(express.json());
+
+        //CORS
+        this.app.use(cors());
+
+        //carpeta publica
+        this.app.use(express.static("public")); 
+    }
+    routes(){
+        this.app.use(this.usuariosPath, require("../routes/usuarios"));
+        this.app.use(this.authPath, require("../routes/auth"));
+
+    }
+
+
+
+    listen(){
         this.app.listen(this.port, ()=>{
-            console.log("Server Online", this.port)
+            console.log("Server online port: ", this.port);
         });
     }
 
