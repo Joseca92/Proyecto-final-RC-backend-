28fd5146050a4bca89d56a72dc45c505713a7c80
Juan <sebas981@live.com>
2022-08-05 22:37:15 -0300

    Creacion de middleware y ruta del menu, creacion de controlador del menu

--- models/server.js
+++ models/server.js
@@ -5,7 +5,14 @@ class Server{
     constructor() {
         this.app = express();
         this.port = process.env.port
+        this.menuPath= '/menu'
         this.conectarDB();
+        this.middleware();
+        this.routes()
+    }
+    middleware(){
+        this.app.use(express.json)
+        
     }
 
     async conectarDB() {
@@ -15,6 +22,9 @@ class Server{
         this.app.listen(this.port, ()=>{
             console.log("Server Online", this.port)
         });
+    }
+    routes(){
+        this.app.use(this.menuPath, require('../routes/menu'))
     }
 
 }
